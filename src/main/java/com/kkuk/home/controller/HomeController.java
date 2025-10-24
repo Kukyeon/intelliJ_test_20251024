b57f7aa5247a1c0a7fd2872fcafbca91c3d42eaf
Kukyeon <rnrdus2000@naver.com>
2025-10-24 12:29:43 +0900

    롬복 테스트 Dto 및 Entity 추가

--- src/main/java/com/kkuk/home/controller/HomeController.java
+++ src/main/java/com/kkuk/home/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.kkuk.home.controller;
 
 
+import com.kkuk.home.dto.Board;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,15 @@ public class HomeController {
     @GetMapping("/test")
     public String test(){
         return "hello world!!!!!";
+    }
+
+    @GetMapping("/board")
+    public Board board(){
+
+        Board board = new Board();
+        board.setName("홍길동");
+        board.setPass("123123");
+
+        return board;
     }
 }
