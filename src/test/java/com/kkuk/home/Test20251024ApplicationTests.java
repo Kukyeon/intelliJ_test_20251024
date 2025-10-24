217207cc3f9bad540395ef318d09fd5d8312ff23
Kukyeon <rnrdus2000@naver.com>
2025-10-24 11:38:39 +0900

    Initial commit

---
+++ src/test/java/com/kkuk/home/Test20251024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkuk.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test20251024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
